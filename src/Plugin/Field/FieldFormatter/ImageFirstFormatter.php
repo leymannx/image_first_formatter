47f90c58c019f95f8531f14a4c35e9b410e0aada
Norman Kämper-Leymann <norman@1xinternet.de>
2021-10-20 15:22:15 +0200

    Properly extend the existing ImageFormatter

--- src/Plugin/Field/FieldFormatter/ImageFirstFormatter.php
+++ src/Plugin/Field/FieldFormatter/ImageFirstFormatter.php
@@ -2,18 +2,10 @@
 
 namespace Drupal\image_first_formatter\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\Core\Field\FieldItemListInterface;
-use Drupal\Core\Field\FieldDefinitionInterface;
-use Drupal\Core\Link;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Session\AccountInterface;
-use Drupal\Core\Url;
-use Drupal\image\Entity\ImageStyle;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Cache\Cache;
-use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Url;
+use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatter;
 
 /**
  * Plugin implementation of the 'image first' formatter.
@@ -23,144 +15,14 @@ use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
  *   label = @Translation("Image First"),
  *   field_types = {
  *     "image"
+ *   },
+ *   quickedit = {
+ *     "editor" = "image"
  *   }
  * )
  */
-class ImageFirstFormatter extends ImageFormatterBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The current user.
-   *
-   * @var \Drupal\Core\Session\AccountInterface
-   */
-  protected $currentUser;
-
-  /**
-   * The image style entity storage.
-   *
-   * @var \Drupal\image\ImageStyleStorageInterface
-   */
-  protected $imageStyleStorage;
-
-  /**
-   * Constructs an ImageFormatter object.
-   *
-   * @param string $plugin_id
-   *   The plugin_id for the formatter.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
-   *   The definition of the field to which the formatter is associated.
-   * @param array $settings
-   *   The formatter settings.
-   * @param string $label
-   *   The formatter label display setting.
-   * @param string $view_mode
-   *   The view mode.
-   * @param array $third_party_settings
-   *   Any third party settings settings.
-   * @param \Drupal\Core\Session\AccountInterface $current_user
-   *   The current user.
-   */
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, AccountInterface $current_user, EntityStorageInterface $image_style_storage) {
-    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
-    $this->currentUser = $current_user;
-    $this->imageStyleStorage = $image_style_storage;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['label'],
-      $configuration['view_mode'],
-      $configuration['third_party_settings'],
-      $container->get('current_user'),
-      $container->get('entity.manager')->getStorage('image_style')
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function defaultSettings() {
-    return [
-      'image_style' => '',
-      'image_link' => '',
-    ] + parent::defaultSettings();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsForm(array $form, FormStateInterface $form_state) {
-    $image_styles = image_style_options(FALSE);
-    $description_link = Link::fromTextAndUrl(
-      $this->t('Configure Image Styles'),
-      Url::fromRoute('entity.image_style.collection')
-    );
-    $element['image_style'] = [
-      '#title' => t('Image style'),
-      '#type' => 'select',
-      '#default_value' => $this->getSetting('image_style'),
-      '#empty_option' => t('None (original image)'),
-      '#options' => $image_styles,
-      '#description' => $description_link->toRenderable() + [
-        '#access' => $this->currentUser->hasPermission('administer image styles'),
-      ],
-    ];
-    $link_types = [
-      'content' => t('Content'),
-      'file' => t('File'),
-    ];
-    $element['image_link'] = [
-      '#title' => t('Link image to'),
-      '#type' => 'select',
-      '#default_value' => $this->getSetting('image_link'),
-      '#empty_option' => t('Nothing'),
-      '#options' => $link_types,
-    ];
-
-    return $element;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function settingsSummary() {
-    $summary = [];
-
-    $image_styles = image_style_options(FALSE);
-    // Unset possible 'No defined styles' option.
-    unset($image_styles['']);
-    // Styles could be lost because of enabled/disabled modules that defines
-    // their styles in code.
-    $image_style_setting = $this->getSetting('image_style');
-    if (isset($image_styles[$image_style_setting])) {
-      $summary[] = t('Image style: @style', ['@style' => $image_styles[$image_style_setting]]);
-    }
-    else {
-      $summary[] = t('Original image');
-    }
-
-    $link_types = [
-      'content' => t('Linked to content'),
-      'file' => t('Linked to file'),
-    ];
-    // Display this setting only if image is linked.
-    $image_link_setting = $this->getSetting('image_link');
-    if (isset($link_types[$image_link_setting])) {
-      $summary[] = $link_types[$image_link_setting];
-    }
-
-    return $summary;
-  }
-
+class ImageFirstFormatter extends ImageFormatter {
+  
   /**
    * {@inheritdoc}
    */
@@ -179,7 +41,7 @@ class ImageFirstFormatter extends ImageFormatterBase implements ContainerFactory
     if ($image_link_setting == 'content') {
       $entity = $items->getEntity();
       if (!$entity->isNew()) {
-        $url = $entity->urlInfo();
+        $url = $entity->toUrl();
       }
     }
     elseif ($image_link_setting == 'file') {
@@ -232,43 +94,6 @@ class ImageFirstFormatter extends ImageFormatterBase implements ContainerFactory
     }
 
     return $elements;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function calculateDependencies() {
-    $dependencies = parent::calculateDependencies();
-    $style_id = $this->getSetting('image_style');
-    /** @var \Drupal\image\ImageStyleInterface $style */
-    if ($style_id && $style = ImageStyle::load($style_id)) {
-      // If this formatter uses a valid image style to display the image, add
-      // the image style configuration entity as dependency of this formatter.
-      $dependencies[$style->getConfigDependencyKey()][] = $style->getConfigDependencyName();
-    }
-    return $dependencies;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function onDependencyRemoval(array $dependencies) {
-    $changed = parent::onDependencyRemoval($dependencies);
-    $style_id = $this->getSetting('image_style');
-    /** @var \Drupal\image\ImageStyleInterface $style */
-    if ($style_id && $style = ImageStyle::load($style_id)) {
-      if (!empty($dependencies[$style->getConfigDependencyKey()][$style->getConfigDependencyName()])) {
-        $replacement_id = $this->imageStyleStorage->getReplacementId($style_id);
-        // If a valid replacement has been provided in the storage, replace the
-        // image style with the replacement and signal that the formatter plugin
-        // settings were updated.
-        if ($replacement_id && ImageStyle::load($replacement_id)) {
-          $this->setSetting('image_style', $replacement_id);
-          $changed = TRUE;
-        }
-      }
-    }
-    return $changed;
   }
 
 }
